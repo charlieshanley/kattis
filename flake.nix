3c54f72feb28df5d32e9068a508dfa812edd33f6
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2023-07-27 10:18:35 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "Shell for solving kattis problems";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      ghc = pkgs.haskellPackages.ghcWithPackages (ps: [
-        ps.QuickCheck
+      ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
+        QuickCheck
       ]);
       inherit (pkgs) callPackage mkShell ghcid cabal-install cabal2nix;
     in
